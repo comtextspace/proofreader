dc68ce4307bc08f102560b2ef97c27913bfc72c7
Anton Kovalev <acetonen@gmail.com>
2023-08-13 00:02:45 +0700

    fix docker

--- books/services/image_actions.py
+++ books/services/image_actions.py
@@ -3,5 +3,5 @@ from PIL import Image
 
 
 def extract_text_from_image(image):
-    # with image.open('rb') as image_file:
+    pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
     return pytesseract.image_to_string(Image.open(image), lang='rus')

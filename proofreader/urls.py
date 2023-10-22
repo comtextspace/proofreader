613217ede613d9e22af4f1eb525f8abd9c57baa1
Anton Kovalev <43474261+Acetonen@users.noreply.github.com>
2023-10-22 13:48:05 +0700

    Refactor nginx.conf and urls.py
    - Remove unnecessary location block in nginx.conf
    - Comment out unused paths in urls.py

--- proofreader/urls.py
+++ proofreader/urls.py
@@ -19,9 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
+    # path("", include("books.urls")),
+    path("", admin.site.urls),
+
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("", include("books.urls")),
+
     path("tz_detect/", include("tz_detect.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

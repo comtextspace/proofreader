b87a988247e7aca4f32d1ae4d4c37ae801585670
Anton Kovalev <43474261+Acetonen@users.noreply.github.com>
2023-10-23 16:49:19 +0700

    Update server script: Use "docker-compose" instead of "docker compose" for down, up, migrate, and collectstatic commands.

--- update_server.sh
+++ update_server.sh
@@ -1,4 +1,4 @@
-docker compose -f docker-compose.yml down
-docker compose -f docker-compose.yml up -d --build
-docker compose -f docker-compose.yml web django python manage.py migrate
-docker compose -f docker-compose.yml web django python manage.py collectstatic --force
+docker-compose -f docker-compose.yml down
+docker-compose -f docker-compose.yml up -d --build
+docker-compose -f docker-compose.yml web django python manage.py migrate
+docker-compose -f docker-compose.yml web django python manage.py collectstatic --force

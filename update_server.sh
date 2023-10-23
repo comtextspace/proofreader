ff32f638414ce280a5b30613f61ba667894e75bc
Anton Kovalev <43474261+Acetonen@users.noreply.github.com>
2023-10-23 17:17:49 +0700

    Refactor docker-compose.yml, settings.py, and update_server.sh
    - Remove the environment variable for POSTGRES_PASSWORD in docker-compose.yml
    - Update the default value for DATABASE_URL in settings.py
    - Modify the commands to execute in update_server.sh to use underscores instead of hyphens

--- update_server.sh
+++ update_server.sh
@@ -1,4 +1,4 @@
 docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml up -d --build
-docker exec proofreader-web-1 python3 manage.py migrate
-docker exec proofreader-web-1 python3 manage.py collectstatic --force
+docker exec proofreader_web_1 python3 manage.py migrate
+docker exec proofreader_web_1 python3 manage.py collectstatic --force

06de61438b5921f85b28d5254e7bd3f4312e1b1f
Anton Kovalev <43474261+Acetonen@users.noreply.github.com>
2023-10-23 12:01:47 +0700

    Refactor URLs for admin and books
    - Commented out the previous paths for "admin/" and "" (books.urls)
    - Added a new path for "" using admin.site.urls
    - Updated the URLs for accounts and authentication

--- proofreader/urls.py
+++ proofreader/urls.py
@@ -19,9 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("books.urls")),
-
+    # path("admin/", admin.site.urls),
+    # path("", include("books.urls")),
+    path("", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
 

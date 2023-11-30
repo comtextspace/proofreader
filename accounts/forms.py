0d26e8474b843e02a1b43e58fd3fa8f01a4172e9
Anton Kovalev <43474261+Acetonen@users.noreply.github.com>
2023-11-30 17:40:55 +0700

    Add code field to CustomUserCreationForm
    This commit adds a new field called "code" to the CustomUserCreationForm class in accounts/forms.py. The code field is a CharField with a label of "Кодовое слово" and a maximum length of 50 characters. It is required for form submission.
    
    The clean() method has been overridden to validate the code field. If the entered code does not match the expected value ("текстология"), a validation error will be raised.
    
    These changes allow users to enter a code word during registration and ensure that it matches the expected value before creating a new user.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,22 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.forms import fields
 
 
 class CustomUserCreationForm(UserCreationForm):
+    code = fields.CharField(label='Кодовое слово', max_length=50, required=True)
+
     class Meta:
         model = get_user_model()
-        fields = UserCreationForm.Meta.fields
+        fields = list(UserCreationForm.Meta.fields) + ["code"]
+
+    def clean(self):
+        super().clean()
+        code = self.cleaned_data["code"]
+        if code.lower() != "текстология":
+            raise fields.ValidationError("Неверное кодовое слово")
+
+        return self.cleaned_data
 
     def save(self, commit=True):
         user = get_user_model().objects.create_user(

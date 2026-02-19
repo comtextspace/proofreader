b99c2f76002cf0327ec088fa2c61bb95823ceedb
Anton Kovalev <43474261+Acetonen@users.noreply.github.com>
2026-02-19 15:48:43 +0700

    Fix frontend editor hotkeys

--- accounts/models.py
+++ accounts/models.py
@@ -12,7 +12,7 @@ from books.models import Page
 
 class CustomUserManager(UserManager):
     def create_user(self, username, email=None, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         user = self._create_user(username, email, password, **extra_fields)
 
